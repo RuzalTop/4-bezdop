b1bcbaf80348520b283244684db1cad32db60785
duper <duper@INFANTERISTRUZ>
2023-03-25 22:30:32 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.column1DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.column2DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.column3DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.column4DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataSet1 = new System.Data.DataSet();
+            this.dataTable1 = new System.Data.DataTable();
+            this.dataColumn1 = new System.Data.DataColumn();
+            this.dataColumn2 = new System.Data.DataColumn();
+            this.dataColumn3 = new System.Data.DataColumn();
+            this.dataColumn4 = new System.Data.DataColumn();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(626, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1 датчик";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(626, 159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2 датчик";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(632, 220);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3 датчик";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(632, 270);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "включить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.column1DataGridViewTextBoxColumn,
+            this.column2DataGridViewTextBoxColumn,
+            this.column3DataGridViewTextBoxColumn,
+            this.column4DataGridViewTextBoxColumn});
+            this.dataGridView1.DataMember = "Table1";
+            this.dataGridView1.DataSource = this.dataSet1;
+            this.dataGridView1.Location = new System.Drawing.Point(49, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(522, 294);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // column1DataGridViewTextBoxColumn
+            // 
+            this.column1DataGridViewTextBoxColumn.DataPropertyName = "Column1";
+            this.column1DataGridViewTextBoxColumn.HeaderText = "Column1";
+            this.column1DataGridViewTextBoxColumn.Name = "column1DataGridViewTextBoxColumn";
+            // 
+            // column2DataGridViewTextBoxColumn
+            // 
+            this.column2DataGridViewTextBoxColumn.DataPropertyName = "Column2";
+            this.column2DataGridViewTextBoxColumn.HeaderText = "Column2";
+            this.column2DataGridViewTextBoxColumn.Name = "column2DataGridViewTextBoxColumn";
+            // 
+            // column3DataGridViewTextBoxColumn
+            // 
+            this.column3DataGridViewTextBoxColumn.DataPropertyName = "Column3";
+            this.column3DataGridViewTextBoxColumn.HeaderText = "Column3";
+            this.column3DataGridViewTextBoxColumn.Name = "column3DataGridViewTextBoxColumn";
+            // 
+            // column4DataGridViewTextBoxColumn
+            // 
+            this.column4DataGridViewTextBoxColumn.DataPropertyName = "Column4";
+            this.column4DataGridViewTextBoxColumn.HeaderText = "Column4";
+            this.column4DataGridViewTextBoxColumn.Name = "column4DataGridViewTextBoxColumn";
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            this.dataSet1.Tables.AddRange(new System.Data.DataTable[] {
+            this.dataTable1});
+            // 
+            // dataTable1
+            // 
+            this.dataTable1.Columns.AddRange(new System.Data.DataColumn[] {
+            this.dataColumn1,
+            this.dataColumn2,
+            this.dataColumn3,
+            this.dataColumn4});
+            this.dataTable1.TableName = "Table1";
+            // 
+            // dataColumn1
+            // 
+            this.dataColumn1.Caption = "Время";
+            this.dataColumn1.ColumnName = "Column1";
+            // 
+            // dataColumn2
+            // 
+            this.dataColumn2.Caption = "Время";
+            this.dataColumn2.ColumnName = "Column2";
+            // 
+            // dataColumn3
+            // 
+            this.dataColumn3.Caption = "Время";
+            this.dataColumn3.ColumnName = "Column3";
+            // 
+            // dataColumn4
+            // 
+            this.dataColumn4.Caption = "Время";
+            this.dataColumn4.ColumnName = "Column4";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn column1DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn column2DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn column3DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn column4DataGridViewTextBoxColumn;
+        private System.Data.DataTable dataTable1;
+        private System.Data.DataColumn dataColumn1;
+        private System.Data.DataColumn dataColumn2;
+        private System.Data.DataColumn dataColumn3;
+        private System.Data.DataColumn dataColumn4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        static int t = 0;
+        static bool tim = false;
+        static double[] viv = new double[3];
+        static double[] tek = new double[3];
+        static int[] per = new int[3];
+        public Form1()
+        {
+            InitializeComponent();
+            
+            timer1 = new Timer();
+            timer1.Interval = 1000; // интервал срабатывания в миллисекундах
+            timer1.Tick += timer1_Tick; // подписка на событие Tick
+            for (int i = 0; i < 3; i++)
+            {
+                if (i == 0)
+                {
+                    per[i] = rand.Next(5, 22);
+                }
+                else if (i == 1)
+                {
+                    per[i] = rand.Next(-2, 9);
+                }
+                if (i == 2)
+                {
+                    per[i] = (int)(rand.NextDouble() * (24.66) - 22.79);
+                }
+                tek[i] = rand.Next(100);
+                viv[i] = tek[i];
+                dataTable1.Columns[i + 1].Caption = "Датчик " + (i + 1).ToString() + " (" + per[i].ToString() + " сек)";
+            }
+            foreach (DataGridViewColumn col in dataGridView1.Columns)
+            {
+                col.HeaderText = dataTable1.Columns[col.HeaderText].Caption;
+            }
+            /*Random rand = new Random();
+            for (int i = 0; i <= 2; i++)
+            {
+                per[i] = rand.Next(9) + 1;
+                tek[i] = rand.Next(100);
+                viv[i] = tek[i];
+                dataTable1.Columns[i + 1].Caption = "Датчик " + (i + 1).ToString() + " (" + per[i].ToString() + " сек)";
+            }
+            foreach (DataGridViewColumn col in dataGridView1.Columns)
+            {
+                col.HeaderText = dataTable1.Columns[col.HeaderText].Caption;
+            }
+            */
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DataRow workrow = dataTable1.NewRow();
+            for (int j = 1; j <= 3; j++)
+            {
+                int ost = 0;
+                if (per[j - 1] != 0)
+                {
+                    ost = (t % per[j - 1]);
+                }
+
+                if (j == 2)
+                {
+                    tek[j - 1] = rand.Next(-2, 9);
+                }
+                else if (j == 3)
+                {
+                    if (ost == 0)
+                    {
+                        viv[j - 1] = rand.NextDouble() * (1.87 - (-22.79)) + (-22.79);
+                        per[j - 1] = (int)(Math.Abs(viv[j - 1]) + 1);
+                    }
+                    tek[j - 1] = rand.Next(per[j - 1]);
+                    if (tek[j - 1] == per[j - 1])
+                    {
+                        tek[j - 1] = viv[j - 1];
+                    }
+                }
+                else
+                {
+                    tek[j - 1] = rand.Next(100);
+                    if (ost == 0)
+                       
+                    {
+                        viv[j - 1] = tek[j - 1];
+                    }
+                }
+                workrow[j] = tek[j - 1].ToString();
+            }
+            workrow[0] = t.ToString();
+            dataTable1.Rows.Add(workrow);
+            dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.RowCount - 1;
+            int i = dataGridView1.RowCount - 1;
+            dataGridView1.Rows[i].Selected = true;
+            t++;
+            /* 
+             Random rand = new Random();
+            DataRow workrow;
+            workrow = dataTable1.NewRow();
+            for (int j = 1; j <= 3; j++)
+            {
+                int ost = (t % per[j - 1]);
+                tek[j - 1] = rand.Next(100);
+                if (ost == 0)
+                {
+                    viv[j - 1] = tek[j - 1];
+                }
+                workrow[j] = tek[j - 1].ToString();//исправление тут
+            }
+            workrow[0] = t.ToString();
+            dataTable1.Rows.Add(workrow);
+            dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.RowCount - 1;
+            int i = dataGridView1.RowCount - 1;
+            dataGridView1.Rows[i].Selected = true;
+            t++;
+            */
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            if (!tim)
+            {
+                tim = true;
+                timer1.Enabled = tim;
+                button4.Text = "Выключить";
+                // при необходимости MessageBox.Show("Система включена");
+            }
+            else
+            {
+                tim = false;
+                timer1.Enabled = tim;
+                button4.Text = "Включить";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!tim)
+            {
+                MessageBox.Show("Система выключена");
+            }
+            else
+            {
+                MessageBox.Show("Датчик 3 значение " + tek[2].ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!tim)
+            {
+                MessageBox.Show("Система выключена");
+            }
+            else
+            {
+                MessageBox.Show("Датчик 2 значение " + tek[1].ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!tim)
+            {
+                MessageBox.Show("Система выключена");
+            }
+            else
+            {
+                MessageBox.Show("Датчик 1 значение " + tek[0].ToString());
+            }
+        }
+    }
+}
+
+
